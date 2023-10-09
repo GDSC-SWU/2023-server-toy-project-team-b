3183b59a45ff7fc15896bbc658cf7ebe648580c2
Yusun Choi <nsuy.ch@gmail.com>
2023-10-10 03:08:37 +0900

    chore: Add initial project files

---
+++ server-toy-project/src/test/java/com/gdsc_teamb/servertoyproject/ServerToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc_teamb.servertoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerToyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
