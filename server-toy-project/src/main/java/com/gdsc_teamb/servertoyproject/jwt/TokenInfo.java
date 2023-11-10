cedae93f82c7cd8c371110ae4b811dbd6ae76617
youjin09222 <youjin09222@gmail.com>
2023-11-10 22:57:04 +0900

    feat: Add TokenInfo (#3)

---
+++ server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/jwt/TokenInfo.java
@@ -0,0 +1,15 @@
+package com.gdsc_teamb.servertoyproject.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+// 클라이언트에 토큰을 보내기 위한 DTO
+public class TokenInfo {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
