6a6d621d1fc77729562e0aef813eaa55ea83a96a
youjin09222 <youjin09222@gmail.com>
2023-11-12 22:17:18 +0900

    feat: Add MemberController (#3)

---
+++ server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/login/MemberController.java
@@ -0,0 +1,26 @@
+package com.gdsc_teamb.servertoyproject.login;
+
+import com.gdsc_teamb.servertoyproject.jwt.TokenInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    public TokenInfo login(@RequestBody MemberLoginRequestDto memberLoginRequestDto) {
+        String memberId = memberLoginRequestDto.getMemberId();
+        String password = memberLoginRequestDto.getPassword();
+        TokenInfo tokenInfo = memberService.login(memberId, password);
+        return tokenInfo;
+    }
+    @PostMapping("/test")
+    public String test() {
+        return "success";
+    }
+}
