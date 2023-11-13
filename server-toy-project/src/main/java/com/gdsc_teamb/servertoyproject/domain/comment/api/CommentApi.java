a3a513e92fe4953a8db161291ca151a2da224f86
Yusun Choi <nsuy.ch@gmail.com>
2023-11-14 01:21:10 +0900

    feat: Add Comment API (#6)

--- server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/domain/comment/api/CommentApi.java
+++ server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/domain/comment/api/CommentApi.java
@@ -2,6 +2,8 @@ package com.gdsc_teamb.servertoyproject.domain.comment.api;
 
 import com.gdsc_teamb.servertoyproject.domain.comment.dto.request.NewCommentReqDto;
 import com.gdsc_teamb.servertoyproject.domain.comment.dto.response.CommentResDto;
+import com.gdsc_teamb.servertoyproject.domain.comment.dto.response.ReadCommentResDto;
+import com.gdsc_teamb.servertoyproject.domain.comment.dto.response.UpdateCommentResDto;
 import com.gdsc_teamb.servertoyproject.domain.comment.service.CommentService;
 import com.gdsc_teamb.servertoyproject.global.common.DataResponseDto;
 import com.gdsc_teamb.servertoyproject.global.common.ResponseDto;
@@ -22,5 +24,29 @@ public class CommentApi {
         CommentResDto commentResDto = commentService.addComment(null, postId, newCommentReqDto);
 
         return ResponseEntity.status(201).body(DataResponseDto.of(commentResDto, 201));
+    }
+
+    // 댓글 조회
+    @GetMapping("/{post-id}")
+    public ResponseEntity<ResponseDto> readComment(@PathVariable("post-id") Long postId) {
+        ReadCommentResDto readCommentResDto = commentService.readComment(postId);
+
+        return ResponseEntity.ok(DataResponseDto.of(readCommentResDto, 200));
+    }
+
+    // 댓글 수정
+    @PatchMapping("/{comment-id}")
+    public ResponseEntity<ResponseDto> updateComment(/*@AuthenticationPrincipal UserEntity user, */@RequestParam("temp") Long userId, @PathVariable("comment-id") Long commentId, @RequestBody @Valid NewCommentReqDto newCommentReqDto) {
+        UpdateCommentResDto updateCommentResDto = commentService.updateComment(null, userId, commentId, newCommentReqDto);
+
+        return ResponseEntity.status(201).body(DataResponseDto.of(updateCommentResDto, 201));
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/{comment-id}")
+    public ResponseEntity<ResponseDto> deleteComment(/*@AuthenticationPrincipal UserEntity user, */@RequestParam("temp") Long userId, @PathVariable("comment-id") Long commentId) {
+        commentService.deleteComment(null, userId, commentId);
+
+        return ResponseEntity.ok(ResponseDto.of(200));
     }
 }
