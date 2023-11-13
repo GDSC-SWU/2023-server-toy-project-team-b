d61c10b27af611b7aa2ae9dd711c88957913d17c
Koominji <mjk2715@naver.com>
2023-11-13 09:29:23 +0900

    feat: Add user api (#7)

--- server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/domain/user/domain/UserEntity.java
+++ server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/domain/user/domain/UserEntity.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -27,12 +29,11 @@ public class UserEntity {
     @NotNull
     private String password;
 
-    @Min(2)
-    @Max(8)
+    @Size(min = 2, max = 8)
     @NotNull
     private String nickname;
 
-    @Max(11)
+    @Size(max = 11)
     @NotNull
     private String phone;
 
@@ -59,5 +60,21 @@ public class UserEntity {
     @Override
     public int hashCode() {
         return Objects.hash(email, nickname);
+    }
+
+    public void updatePassword(PasswordEncoder passwordEncoder, String password){
+        this.password = passwordEncoder.encode(password);
+    }
+    public void updatePhone(String phone){
+        this.phone = phone;
+    }
+
+    public void updateNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+    //비밀번호 변경, 회원 탈퇴 시, 비밀번호를 확인하며, 이때 비밀번호의 일치여부를 판단하는 메서드
+    public boolean matchPassword(PasswordEncoder passwordEncoder, String checkPassword){
+        return passwordEncoder.matches(checkPassword, getPassword());
     }
 }
