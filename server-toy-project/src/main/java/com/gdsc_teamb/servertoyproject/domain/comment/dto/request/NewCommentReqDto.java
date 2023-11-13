9e1dfa7d715668b4ff261b56ea2c085650d263a0
Yusun Choi <nsuy.ch@gmail.com>
2023-11-14 01:20:34 +0900

    fix: Add `@NoArgsConstructor` on NewCommentReqDto to deserialize json (#6)

--- server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/domain/comment/dto/request/NewCommentReqDto.java
+++ server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/domain/comment/dto/request/NewCommentReqDto.java
@@ -3,7 +3,9 @@ package com.gdsc_teamb.servertoyproject.domain.comment.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class NewCommentReqDto {
