35a523d5d9de861be41c606ebf4b2f5717455bc7
youjin09222 <youjin09222@gmail.com>
2023-11-13 18:47:31 +0900

    refactor: Modify JwtToken (#3)

--- server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/jwt/JwtToken.java
+++ server-toy-project/src/main/java/com/gdsc_teamb/servertoyproject/jwt/JwtToken.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 // 클라이언트에 토큰을 보내기 위한 DTO
-public class TokenInfo {
+public class JwtToken {
     private String grantType;
     private String accessToken;
     private String refreshToken;
