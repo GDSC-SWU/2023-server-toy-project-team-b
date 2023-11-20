645dd12294454faada46093956b5ca954eb0cca2
nanna29 <luna2907@naver.com>
2023-11-21 01:02:43 +0900

    test: Modify findAll() method to select data by searching for user (#5)

--- server-toy-project/src/test/java/com/gdsc_teamb/servertoyproject/controller/BoardControllerTest.java
+++ server-toy-project/src/test/java/com/gdsc_teamb/servertoyproject/controller/BoardControllerTest.java
@@ -8,10 +8,7 @@ import com.gdsc_teamb.servertoyproject.domain.user.domain.UserEntity;
 import com.gdsc_teamb.servertoyproject.dto.boardDto.BoardDto;
 import com.gdsc_teamb.servertoyproject.dto.boardDto.BoardUpdateDto;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,15 +18,21 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.util.AssertionErrors.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -50,12 +53,14 @@ class BoardControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    final String TITLE = "title-test";
-    final String CONTENT = "content-test";
+
 
     @Test
     @DisplayName("Post 등록 테스트")
     public void postRegister() throws Exception{
+        final String TITLE = "title-test";
+        final String CONTENT = "content-test";
+
         //Given 등록할 게시글 생성
         UserEntity savedUser = userRepository.save(UserEntity.builder()
                 .email("abc@abc.com")
@@ -76,14 +81,18 @@ class BoardControllerTest {
         mockMvc.perform(post(url)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(boardDto)))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(print());
 
         // then
-        // 데이터베이스에 저장된 모든 게시글을 조회하고, 첫 번째 게시글이 기대한 대로 생성되었는지 검증
-        List<PostEntity> all = postRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(TITLE);
-        assertThat(all.get(0).getContent()).isEqualTo(CONTENT);
-        assertThat(all.get(0).getUser().getId()).isEqualTo(savedUser.getId());
+        // 저장한 데이터만 조회
+        List<PostEntity> savedPosts = postRepository.findAllByUser(savedUser);
+        assertThat(savedPosts).hasSize(1); // 사용자에게 속하는 게시글 중에서 검사
+        PostEntity postSaved = savedPosts.get(0);
+
+        assertThat(postSaved.getTitle()).isEqualTo(TITLE);
+        assertThat(postSaved.getContent()).isEqualTo(CONTENT);
+        assertThat(postSaved.getUser().getId()).isEqualTo(savedUser.getId());
 
     }
 
@@ -125,9 +134,14 @@ class BoardControllerTest {
                 .andExpect(status().isOk());
 
         // then
-        List<PostEntity> all = postRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+        // 저장한 데이터만 조회
+        List<PostEntity> savedPosts = postRepository.findAllByUser(savedUser);
+        assertThat(savedPosts).hasSize(1); // 사용자에게 속하는 게시글 중에서 검사
+        PostEntity postSaved = savedPosts.get(0);
+
+        assertThat(postSaved.getTitle()).isEqualTo(expectedTitle);
+        assertThat(postSaved.getContent()).isEqualTo(expectedContent);
+
     }
 
     @Test
